089e288e1a7ceece92af1e48442d373ac15cf9fc
Dawa Lama <dawalama@gmail.com>
2015-04-28 14:27:34 -0400

    Added a very basic search which only does filter on the list right now

--- public/js/app.js
+++ public/js/app.js
@@ -68,4 +68,45 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/section/food');
-});
+})
+.directive('ionSearch', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                getData: '&source',
+                model: '=?',
+                search: '=?filter'
+            },
+            link: function(scope, element, attrs) {
+                attrs.minLength = attrs.minLength || 0;
+                scope.placeholder = attrs.placeholder || '';
+                scope.search = {value: ''};
+
+                if (attrs.class)
+                    element.addClass(attrs.class);
+
+                if (attrs.source) {
+                    scope.$watch('search.value', function (newValue, oldValue) {
+                        if (newValue.length > attrs.minLength) {
+                            scope.getData({str: newValue}).then(function (results) {
+                                scope.model = results;
+                            });
+                        } else {
+                            scope.model = [];
+                        }
+                    });
+                }
+
+                scope.clearSearch = function() {
+                    scope.search.value = '';
+                };
+            },
+            template: '<div class="item-input-wrapper">' +
+                        '<i class="icon ion-android-search"></i>' +
+                        '<input type="search" placeholder="{{placeholder}}" ng-model="search.value">' +
+                        '<i ng-if="search.value.length > 0" ng-click="clearSearch()" class="icon ion-close"></i>' +
+                      '</div>'
+        };
+    });
+

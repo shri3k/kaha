b9cd30173820a2a29e91dd54c0de4b0716569009
pniraula <prabeshniraula@gmail.com>
2015-04-28 00:53:36 -0400

    cleaned

--- public/js/app.js
+++ public/js/app.js
@@ -49,5 +49,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/section/supplies');
+  $urlRouterProvider.otherwise('/app/section/food');
 });

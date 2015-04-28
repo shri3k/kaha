86049f982195b83843b9c3fa1c621568e28fe4a2
pniraula <prabeshniraula@gmail.com>
2015-04-27 23:36:54 -0400

    added frontend

---
+++ views/js/services.js
@@ -0,0 +1,71 @@
+angular.module('starter.services', [])
+.factory("api", function($q, $http){
+	return{
+		data:function(refresh){
+			var url = "";
+			var def = $q.defer();
+			if(refresh){
+				$http.get(url).success(function(data){
+                    def.resolve({success:true, content:JSON.parse(data)})
+            	}).error(function(data, status, headers, config) {
+                    def.resolve({success:false, message:"No data found"})
+            	});
+			}else{
+				var data = JSON.parse(localStorage.getItem("data"));
+				def.resolve({success:true, content: data})
+			}			
+            return def.promise;
+		},
+		filter:{
+			type:function(data, type){
+				var filtered = [];
+				angular.forEach(data, function(item) {
+			      if(type.toUpperCase() == item.type.toUpperCase()) {
+			        filtered.push(item);
+			      }
+			    });
+			    return filtered;
+			}, 
+			location:{
+				district:function(data, type, name){
+					var filtered = [];
+					angular.forEach(data, function(item) {
+					  if(item.location.district.toUpperCase() == name.toUpperCase() && item.type.toUpperCase() == type.toUpperCase()){
+					  	filtered.push(item);
+					  }
+				    });
+				    return filtered;
+				},
+				tole:function(data, type, name){
+					var filtered = [];
+					angular.forEach(data, function(item) {
+					  if(item.location.tole.toUpperCase() == name.toUpperCase() && item.type.toUpperCase() == type.toUpperCase()){
+					  	filtered.push(item);
+					  }
+				    });
+				    return filtered;
+				}
+			}
+		},
+		location:{
+			districts:function(data, type){
+				var filtered = [];
+				angular.forEach(data, function(item) {
+				  if(filtered.indexOf(item.location.district)==-1 && item.type.toUpperCase() == type.toUpperCase()){
+				  	filtered.push(item.location.district);
+				  }
+			    });
+			    return filtered;
+			},
+			tole:function(data, type, district){
+				var filtered = [];
+				angular.forEach(data, function(item) {
+				  if(item.location.district.toUpperCase() == district.toUpperCase() && item.type.toUpperCase() == type.toUpperCase()){
+			      		filtered.push(item.location.tole);
+				  }
+			    });
+			    return filtered;
+			}
+		}
+	}
+});

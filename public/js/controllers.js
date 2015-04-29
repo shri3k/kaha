105f941b213a69735f4c64ce904064290b2c1614
pniraula <prabeshniraula@gmail.com>
2015-04-28 20:44:13 -0400

    added services to add edit and update data

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -69,10 +69,13 @@ angular.module('starter.controllers', [])
 
 .controller('ItemCtrl', function($scope, $stateParams, $rootScope, api) {
   $scope.markAsUnavailable = function(){
-    api.markAsUnavailable();
+    api.markAsUnavailable($rootScope.selectedItem);
+  }
+  $scope.markHelpfull = function(){
+    api.markHelpfull($rootScope.selectedItem);
   }
   $scope.requestRemove = function(){
-    api.requestRemove();
+    api.requestRemove($rootScope.selectedItem);
   }
   $scope.editItem = function(){
     window.location = "#/app/edit";
@@ -110,7 +113,7 @@ angular.module('starter.controllers', [])
         error = true;
       }
       if(!error){
-        api.submit($rootScope.selectedItem).then(function(data){
+        api.update($rootScope.selectedItem).then(function(data){
           alert("submitted")
         });
       }

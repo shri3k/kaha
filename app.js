ef62bb701b2aed4c4f8f8ae9d1e7b98e1d8ef371
yojan shrestha <sickgrin@gmail.com>
2015-04-29 17:56:13 -0500

    Added cors for public access

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var routes = require('./routes/index');
 
 var app = express();
 
+//Cors Headers
+var CORS = function(req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
@@ -25,7 +31,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(CORS);
 app.use('/', routes);
 
 // catch 404 and forward to error handler

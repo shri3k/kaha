37704278b1dcb3f9f1f6217267b0cd8d41c7de81
pniraula <prabeshniraula@gmail.com>
2015-04-29 22:33:22 -0400

    prevent page from going back to root after submit or edit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -101,7 +101,7 @@ angular.module('starter.controllers', [])
 .controller('AboutCtrl', function($scope, $stateParams, $rootScope) {
 
 })
-.controller('SubmitCtrl', function($scope, api, $stateParams, $ionicHistory) {
+.controller('SubmitCtrl', function($scope, api, $stateParams, $ionicHistory, $window) {
     $scope.formTitle = 'New Supply/Resource';
     $scope.submitdata = {
         channel:'supply',
@@ -201,7 +201,7 @@ angular.module('starter.controllers', [])
                 api.submit(data).then(function(data){
                     if(data){
                         alert("Successfully Updated");
-                        $scope.submitdata = {};
+                        $window.location.reload();
                     }
                 });
             }

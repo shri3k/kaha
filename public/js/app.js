90beb10aee63bf031befb2dcdda2cc2a1cbe6328
Dawa Lama <dawalama@gmail.com>
2015-05-05 10:52:48 -0400

    Fix the filter tagging, and improve result by adding hashtag since need or supply can readily occur in description

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
+
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
